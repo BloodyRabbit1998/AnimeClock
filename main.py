d6c9053728404605f50576a29e12b895be7ad165
BloodyRabbit <cool.goodboy1998@gmail.com>
2022-02-19 11:59:03 +0600

    Доработан интерфейс

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty,ObjectProperty
 from kivymd.uix.card import MDCardSwipe
 from kivymd.uix.picker import MDTimePicker
+from kivymd_extensions.akivymd.uix.bottomappbar import AKFloatingRoundedAppbar, AKFloatingRoundedAppbarButtonItem
+
 Builder.load_file('main.kv')
 
 class SwipeToDeleteItem(MDCardSwipe):
     text = StringProperty()
+    on_delete=ObjectProperty()
     def remove_item(self, instance):
         self.screen.ids.md_list.remove_widget(instance)
 class MainScreen(MDScreen):
@@ -17,22 +20,27 @@ class MainScreen(MDScreen):
 
 class TimeScreen(MDScreen):
     def remove_item(self, instance):
-        self.screen.ids.list_time.remove_widget(instance)
-    def get_time(self,instance,time):
-        print(time)
+        print("delete")
+        self.ids.list_time.remove_widget(instance)
     def save(self,instance,time):
+        
         from datetime import datetime
-        self.ids.list_time.add_widget(SwipeToDeleteItem(text=str(time)))
+        self.ids.list_time.add_widget(SwipeToDeleteItem(text=str(time),on_delete=lambda x:self.remove_item()))
     def add_alarm(self):		
         time_dialog = MDTimePicker()
-        time_dialog.bind(on_save=self.save,time=self.get_time)
+        time_dialog.bind(on_save=self.save)
         time_dialog.open()
 
 class AlarmApp(MDApp):
+
+    def remove_item(self, instance):
+        print("delete")
+        self.mainScreen.ids.list_time.remove_widget(instance)
     def __init__(self, **kwargs):
         super(AlarmApp, self).__init__(**kwargs)
     def build(self):
-        return MainScreen()
+        self.mainScreen=MainScreen()
+        return self.mainScreen
 
 
 def main():
